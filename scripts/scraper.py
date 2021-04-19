5ce9c39e81b7a6c58a42c3d642a56f7611484224
Sean Hicks <shicks@stevens.edu>
2021-04-19 16:27:50 -0500

    got ingredient scraping up, still working on export

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,2 +1,11 @@
 import requests as rq
 from bs4 import BeautifulSoup
+page = rq.get('https://tasty.co/recipe/hainanese-chicken-rice')
+# requests site
+soup = BeautifulSoup(page.content, "lxml")
+# parses HTML using lxml parser
+
+for listItem in soup.find_all(class_="ingredient xs-mb1 xs-mt0"):
+    # iterates through elements containing ingredients
+    listing = listItem.text
+    print(listing)
