51f11fafdcffafd23493236a573f12970cce54fa
Sean Hicks <shicks@stevens.edu>
2021-04-20 20:37:26 -0500

    finished scraper, search still unsolved

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,11 +1,15 @@
 import requests as rq
 from bs4 import BeautifulSoup
-page = rq.get('https://tasty.co/recipe/hainanese-chicken-rice')
-# requests site
-soup = BeautifulSoup(page.content, "lxml")
-# parses HTML using lxml parser
 
-for listItem in soup.find_all(class_="ingredient xs-mb1 xs-mt0"):
-    # iterates through elements containing ingredients
-    listing = listItem.text
-    print(listing)
+
+def getIngredientsTasty(site):
+    page = rq.get(site)
+    # requests site
+    soup = BeautifulSoup(page.content, "lxml")
+    # parses HTML using lxml parser
+    listing = []
+    for listItem in soup.find_all(class_="ingredient xs-mb1 xs-mt0"):
+        # iterates through elements containing ingredients
+        listing.append(listItem.text)
+    # print can be subbed for sending to whatever docs are needed. Join will put the list of items into line separated.
+    print('\n'.join(listing))
