e3b0738f22178a0a4bfe0709d055c4df43b7c5f5
Sean Hicks <shicks@stevens.edu>
2021-05-02 04:02:44 -0500

    finally figured out calling (maybe, testing needed)

--- scripts/callScraper.js
+++ scripts/callScraper.js
@@ -1,7 +1,11 @@
-$.ajax({
-  type: "POST",
-  url: "scraper.py",
-  data: { param: site },
-}).done(function (o) {
-  // do something
+//for fopy pasting with parameters
+$.getJSON("link", function (data, textStatus, jqXHR) {
+  //I believe we swap data for the search we want
+  var results = [];
+  $.each(data, function (val) {
+    results.push("<li id = 'id'>" + val + "</li>");
+  });
+  $("<ul/>", { class: "class", html: results.join("") }).appendTo(
+    document.body
+  ); //document.body can be swapped for some destination I think
 });
--- scripts/callSearch.js
+++ scripts/callSearch.js
@@ -1,7 +1,11 @@
-$.ajax({
-  type: "POST",
-  url: "search.py",
-  data: { param: searchTerms },
-}).done(function (o) {
-  // do something
+//just for copy pasting probably
+$.getJSON("/search?terms=x", function (data, textStatus, jqXHR) {
+  //I believe we swap data for the search we want
+  var results = [];
+  $.each(data, function (val) {
+    results.push("<li id = 'searchResultTitle'>" + val + "</li>");
+  });
+  $("<ul/>", { class: "searchResults", html: results.join("") }).appendTo(
+    document.body
+  ); //document.body can be swapped for some destination I think
 });
