0dbaffd2156a2943c19c9a8cc171973d9cbfca9e
Sean Hicks <shicks@stevens.edu>
2021-05-04 00:34:43 -0500

    final touches, Bootstrap Theme Added

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, send_file
 from flask_login import login_required, current_user
 from .search import getResultsTasty
 from .scraper import *
@@ -23,12 +23,20 @@ def home():
                     lambda x: "<form method='POST' site_title='" + x + "'><button type='submit' class='btn btn-primary'>" + x + "</button><br /></form>", titleList))
                 return render_template('home.html', user=current_user, siteButtons=intoButtons, siteTitlesDict=searchTermsDict)
         else:
-            # siteToParse = 'tasty.co' + \
-            #     searchTermsDict[request.form.get('id')]
-            # print(siteToParse)
+            # siteAddition = searchTermsDict[request.form.get('id')]
+            # siteAddition.strip()
+            # siteAddition = siteAddition.replace('(', '')
+            # siteAddition = siteAddition.replace(')', '')
+            # siteAddition = siteAddition.replace(',', '')
+            # siteAddition = siteAddition.replace("'", '')
+            # siteAddition = siteAddition.replace(' ', '-')
+            # siteToParse = 'tasty.co' + siteAddition
             # manipulate string so no apostrophes or parenthesis then join with -
-            siteToParseTest = 'https://tasty.co/recipe/asian-chicken-chopped-salad'
-            return redirect(url_for('views.viewRecipe', siteLink=siteToParseTest))
+
+            # WORKAROUND. request.form.get('id') returns None for reasons I cannot explain,
+            # if it correctly returned a string the commented out code above would work:
+            siteToParse = 'https://tasty.co/recipe/asian-chicken-chopped-salad'
+            return redirect(url_for('views.viewRecipe', siteLink=siteToParse))
     return render_template('home.html', user=current_user)
 
 
@@ -38,5 +46,15 @@ def viewRecipe(siteLink):
     title = getTitleTasty(siteLink)
     ingredientsList = getIngredientsTasty(siteLink)
     instructions = getInstructionsTasty(siteLink)
+    file = open('shopping-list.txt', 'w', encoding='utf-8')
+    for number, ingredient in enumerate(ingredientsList):
+        file.write(f"{number}: {ingredient}\n")
+    file.close()
 
     return render_template("viewrecipe.html", siteTitle=title, ingredients=ingredientsList, instructions=instructions, user=current_user)
+
+
+@views.route('/download')
+def download():
+    path = "shopping-list.txt"
+    return send_file(path, as_attachment=True)
