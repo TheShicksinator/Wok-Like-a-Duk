346b659d6de372b6c93ae51f6e28c7db65afe51c
Sean Hicks <shicks@stevens.edu>
2021-05-03 19:50:54 -0500

    working search result buttons(maybe)

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 from .search import getResultsTasty
 views = Blueprint('views', __name__)
@@ -7,13 +7,25 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    searchTermsDict = {}
     if request.method == 'POST':
-        search = request.form.get('search')
-        if len(search) < 1:
-            flash('Please enter search terms before submitting', category='error')
+        if "search-button" in request.form:
+            search = request.form.get('search')
+            if len(search) < 1:
+                flash('Please enter search terms before submitting',
+                      category='error')
+            else:
+                flash('Search Successful', category='success')
+                searchTermsDict = getResultsTasty(search)
+                titleList = list(searchTermsDict.keys())
+                intoButtons = list(map(
+                    lambda x: "<button class=\"list-group-item\" id=" + x + ">" + x + "</button>", titleList))
+                print(intoButtons)
+                return render_template('home.html', user=current_user, siteButtons=intoButtons, siteTitlesDict=searchTermsDict)
         else:
-            flash('Search Successful', category='success')
-            searchTermsDict = getResultsTasty(search)
-            titleList = list(searchTermsDict.keys())
-            return render_template('home.html', user=current_user, siteTitles=titleList)
+            siteToParse = 'tasty.co' + searchTermsDict[request.form.get('id')]
+            print(siteToParse)
     return render_template('home.html', user=current_user)
+
+
+@views.route('/view-recipe/')
